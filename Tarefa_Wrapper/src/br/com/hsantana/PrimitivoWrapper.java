86e6231028ab8054568cc5ec829f807cfb1c4a39
Higor Santana <137120585+higorsantana96@users.noreply.github.com>
2023-12-30 21:53:00 -0300

    Add files via upload

---
+++ Tarefa_Wrapper/src/br/com/hsantana/PrimitivoWrapper.java
@@ -0,0 +1,11 @@
+package br.com.hsantana;
+
+public class PrimitivoWrapper {
+
+    public static void main(String args[]) {
+        int num1 = 10203040;
+        System.out.println(num1);
+        int num2 = Integer.valueOf(num1);
+        System.out.println(num2);
+    }
+}
